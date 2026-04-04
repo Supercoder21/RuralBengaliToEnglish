c9b8674aac4a97783e5d01b382b623f2a86050c5
Aarav Trivedi <69123056+Supercoder21@users.noreply.github.com>
2026-04-04 11:21:16 -0400

    Update perplexity.py

--- eval/perplexity.py
+++ eval/perplexity.py
@@ -1,1 +1,15 @@
+import math
+def compute_perplexity(sentence, bigram_probs):
+    words =["<s>"] + sentence.split()
+    log_sum=0
+    T=len(words) - 1
+    for i in range(1, len(words)):
+        prev_word= words[i - 1]
+        curr_word=words[i]
 
+        prob=bigram_probs.get((prev_word, curr_word))
+        prior_log_sum =log_sum
+        log_sum +=math.log(prob)
+
+    perplexity= math.exp(-log_sum / T)
+    return perplexity
