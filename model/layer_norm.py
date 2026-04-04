6a765fecddbd104fb2274e40b152a4ffd87ae983
Aarav Trivedi <69123056+Supercoder21@users.noreply.github.com>
2026-04-04 11:24:35 -0400

    Update layer_norm.py

--- model/layer_norm.py
+++ model/layer_norm.py
@@ -1,1 +1,9 @@
+import numpy as np
+import math
 
+def layer_norm(x, weights, bias, epsilon=10**-6):
+    x_mean = np.mean(x)
+    x_variance = np.var(x)
+    x_norm = (x - x_mean)/np.sqrt(x_variance + epsilon) #epsilon used to stop DivisionByZero errors
+    x_output = weights * x_norm + bias
+    return x_output    
